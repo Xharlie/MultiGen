585c6931675d9dc03dc5b72fa05f9c89d8a8acee
xharlie <charlie.xu007@yahoo.com>
2017-04-17 23:32:11 +0800

    change GCN

--- train_GCN/large/GCN_train_large.sh
+++ train_GCN/large/GCN_train_large.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-/Users/xharlie/caffe/build/tools/caffe train --solver=GCN_solver_large.prototxt  $@ # -weights alex_3797.caffemodel $@
+/home/xharlie/dev/caffe/build/tools/caffe train --solver=GCN_solver_large.prototxt   -weights 995_multi_snap_alex_4emo__iter_30000.caffemodel $@
