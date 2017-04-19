77f95e996ec7b06959a6fa8afab44d7acbe5e03d
xharlie <charlie.xu007@yahoo.com>
2017-04-19 21:56:15 +0800

    flex

--- train_GCN/large/GCN_train_large.sh
+++ train_GCN/large/GCN_train_large.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-/home/xharlie/dev/caffe/build/tools/caffe train --solver=GCN_solver_large.prototxt   -weights 995_multi_snap_alex_4emo__iter_30000.caffemodel $@
+/home/xharlie/dev/caffe/build/tools/caffe train --solver=GCN_solver_large.prototxt $@ # -weights genOnlySnapshot_iter_20000.caffemodel,995_multi_snap_alex_4emo__iter_30000.caffemodel $@
